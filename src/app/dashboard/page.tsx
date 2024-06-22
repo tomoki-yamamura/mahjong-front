91c5df791c92537be3d27d86d0c5fec3c21e3d25
tomoki-yamamura <progtab17@gmail.com>
2024-06-22 19:30:56 +0800

    tablists

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ export default function Dashboard() {
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Dashboard</h1>
         <div className="flex items-center gap-3">
-          <DatePickerWithRange/>
+          <DatePickerWithRange />
           <Button>
             Submit
           </Button>
