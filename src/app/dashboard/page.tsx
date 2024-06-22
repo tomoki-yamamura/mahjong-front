e0c44cdbb0b17ce7d254703b6d1482c432c8b264
tomoki-yamamura <progtab17@gmail.com>
2024-06-22 17:54:54 +0800

    Dashboard init

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { DatePickerWithRange } from "@/components/ui/dateRangePicker";
+
 export default function Dashboard() {
   return (
-    <div>page</div>
+    <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <div className="flex items-center gap-3">
+          <DatePickerWithRange/>
+          <Button>
+            Submit
+          </Button>
+        </div>
+      </div>
+      <div>here is section dashboard</div>
+    </div>
   )
 }
